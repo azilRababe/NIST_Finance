0fdbd7ee46c1a0dd1d9db67ca93bb09f5df3ddf6
azilrababe <azilrababe@gmail.com>
2023-03-25 16:52:02 +0000

    Fix:nodemailer- OAuth

--- sever/utils/sendEmail.js
+++ sever/utils/sendEmail.js
@@ -1,23 +1,30 @@
 import nodemailer from "nodemailer";
 
 export const sendEmail = (email, subject, text) => {
-  // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: process.env.USER,
-      pass: process.env.PASS,
+      type: "OAuth2",
+      user: process.env.MAIL_USERNAME,
+      pass: process.env.MAIL_PASSWORD,
+      clientId: process.env.OAUTH_CLIENTID,
+      clientSecret: process.env.OAUTH_CLIENT_SECRET,
+      refreshToken: process.env.OAUTH_REFRESH_TOKEN,
     },
   });
 
-  // send mail with defined transport object
-  transporter
-    .sendMail({
-      from: process.env.USER,
-      to: email,
-      subject: subject,
-      text: text,
-    })
-    .then(() => console.log("Email send successfully"))
-    .catch((err) => console.log({ err: err }));
+  let mailOptions = {
+    from: process.env.MAIL_USERNAME,
+    to: email,
+    subject: subject,
+    text: text,
+  };
+
+  transporter.sendMail(mailOptions, function (err, data) {
+    if (err) {
+      console.log("Error " + err);
+    } else {
+      console.log("Email sent successfully");
+    }
+  });
 };
