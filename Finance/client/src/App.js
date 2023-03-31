69579c50405b12b4eb1e719d76d22543eb07384d
azilrababe <azilrababe@gmail.com>
2023-03-31 23:45:09 +0000

    fix: user_auth

---
+++ Finance/client/src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import { Login } from './pages/Login';
+import { Register } from './pages/Register';
+import { ResetPassword } from './pages/ResetPassword';
+import { ForgotPassword } from './pages/ForgotPassword';
+import { Home } from './pages/Home';
+
+import { useIsAuthenticated } from 'react-auth-kit';
+
+function App() {
+  const isAuthenticated = useIsAuthenticated();
+
+  const router = createBrowserRouter([
+    {
+      path: '/login',
+      element: isAuthenticated() ? <Home /> : <Login />,
+    },
+    {
+      path: '/register',
+      element: <Register />,
+    },
+    {
+      path: '/ForgotPassword',
+      element: <ForgotPassword />,
+    },
+    {
+      path: '/ResetPassword',
+      element: <ResetPassword />,
+    },
+  ]);
+  return (
+    <ChakraProvider>
+      <RouterProvider router={router} />
+    </ChakraProvider>
+  );
+}
+
+export default App;
