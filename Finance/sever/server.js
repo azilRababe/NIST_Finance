661c2ae107d251052606db3de4b6e1a15d40f4aa
azilrababe <azilrababe@gmail.com>
2023-03-31 23:45:35 +0000

    fix: user_auth

---
+++ Finance/sever/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+const app = express();
+
+import dotenv from "dotenv";
+dotenv.config();
+
+import helmet from "helmet";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+import "./utils/mongodb.js"; //Database
+
+import auth from "./routes/auth.js";
+
+// middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+app.use(helmet());
+
+// ROUTES
+app.use("/api/auth", auth);
+
+const Port = process.env.PORT || 8080; //PORT
+app.listen(Port, () => console.log(`App running on port ${Port}`));
